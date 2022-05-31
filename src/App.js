6bfeda21355673ee0e91f08afd3bab6e924514c9
Vikas Rathod <vikas.rv4299@gmail.com>
2022-05-31 17:38:40 +0530

    dependency installed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
+import {useAuthState} from "react-firebase-hooks/auth"
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDyu43tHCk6GRW1cL3OHhHk_bat0OMaA8Y",
+  authDomain: "chat-app-e5954.firebaseapp.com",
+  projectId: "chat-app-e5954",
+  storageBucket: "chat-app-e5954.appspot.com",
+  messagingSenderId: "769554908925",
+  appId: "1:769554908925:web:26f8080b64d8812f632094"
+})
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+
+  const messageRef = firestore.collection('massages')
+  const [user] =useAuthState(auth)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
